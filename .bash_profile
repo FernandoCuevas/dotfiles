38dd981a74f23f168216905e27f080b15acd2d5f
fcuevas <fcuevas@tripadvisor.com>
2018-10-25 09:41:47 +1100

    update

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,11 @@
-###PERSONAL###
+####PERSONAL####
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+# add support for ctrl+o to open selected file in VS Code
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
+
+###alias###
+alias ip="ifconfig | grep -Eo \"([0-9]{1,3}\.){3}[0-9]{1,3}\""
+alias cat='bat'
 
 ##cd##
 alias ..="cd .."
@@ -12,3 +19,8 @@ alias gb="git branch"
 alias gs="git status"
 alias gd="git diff"
 alias ga="git add"
+
+###load bashrc###
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+fi
--- setup.sh
+++ setup.sh
@@ -23,5 +23,32 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 source ~/.bash_profile
 nvm install 9
 nvm install 10
+#get bat
+brew install bat
+#get gzg
+brew install fzf
+yes | $(brew --prefix)/opt/fzf/install
+#get diff-so-fancy
+brew install diff-so-fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+#get tldr
+npm install -g tldr
+#get cowsay
+brew install cowsay
+cowsay done
 
 #TODO key management
